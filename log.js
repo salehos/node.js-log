6b8ee16c22ea210516881b6ea08dcfae5d0587a7
salehos <salehos763@gmail.com>
2021-05-11 22:51:31 +0430

    Fix format for date and time and log_level and message. module is left...

--- log.js
+++ log.js
@@ -1,42 +1,53 @@
 async function log(message, log_level, log_name) {
 	var dateFormat = require('dateformat');
 	const { createLogger, format, transports } = require('winston');
-	var day = dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
-
+	const { combine, timestamp, label, printf } = format;
+	var day = dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss:l");
+	
 	if(log_name === undefined){
-		log_name = "log"
-	}
+			log_name = "log"
+		}
 
+	const myFormat = printf(({ level, message, label, timestamp }) => {
+ 	 	return `${timestamp} ${level} [${label}] : ${message}`;
+	});
+
+	
 	const logger = createLogger({
-	  format: format.combine(
-	    format.splat(),
-	    format.simple()
-	  ),
-	  transports: [new transports.File({
+	  	format: combine(
+    		label({ label: 'right meow!' }),
+    		timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+    		myFormat
+  		),
+	  	transports: [new transports.File({
       filename: log_name+".log",
     }),]
 	});
 	
 	console.log("%s", log_name)
 	if (log_level==="error"){
-		logger.log('error', 'at %s %s',day ,message);
+		logger.log('error',message);
 	}
 	else if (log_level === "warning"){
-		logger.log('warning', 'at %s %s',day ,message);
+		logger.log('warning',message);
 	}
 	else if (log_level === "crit"){
-		logger.log('crit', 'at %s %s',day ,message);
+		logger.log('crit',message);
 	}
 	else if (log_level === "debug"){
-		logger.log('debug', 'at %s %s',day ,message);
+		logger.log('debug',message);
 	}
 	else if (log_level === "notice"){
-		logger.log('notice', 'at %s %s',day ,message);
+		logger.log('notice' ,message);
 	}
 	else if (log_level === "emerg"){
-		logger.log('emerg', 'at %s %s',day ,message);
+		logger.log('emerg',message);
 	}
 	else{
-		logger.log('info', 'at %s %s',day ,message);
+		logger.log(level='info', message);
 	}
 }
+
+log("hello")
