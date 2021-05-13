4b8a2e786f3b9b6963bc4918c822711f0fea84ed
salehos <salehos763@gmail.com>
2021-05-13 06:32:25 +0430

    Tested log.js file and produce and consume some SHITS

--- log.js
+++ log.js
@@ -1,54 +1,60 @@
 const { Kafka } = require('kafkajs')
-const kafka = new Kafka({
-    clientId: clientId,
-    brokers: [ipaddress:port]
-})
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf } = format;
 var dateFormat = require('dateformat');
 
-async function producer(message, log_level) {
-    const producer = kafka.producer()
-    var day = dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss:l");
-    await producer.connect()
-    await producer.send({
-        topic: 'log',
-        log_level: log_level,
-        module_name: global.process.mainModule.filename,
-        messages: message,
-    })
-    await producer.disconnect()
+
+class Log{
+	constructor(clientId, brokers, module_name){
+		this.clientId = clientId;
+		this.brokers = brokers;
+		this.moduleName = module_name;
+	}
+
+	// async produce_test(message, log_level) {
+		
+	// }
+
+	async log(message, log_level, log_name) {		
+		if(log_name === undefined){
+				log_name = "log"
+			}
+		if(log_level === undefined){
+				log_level = "info"
+			}
+		const myFormat = printf(({ level, message, timestamp }) => {
+	 	 	return `${timestamp} ${level} : ${message}`;
+		});
+
+		
+		const logger = createLogger({
+		  	format: combine(
+	    		timestamp({
+	            format: 'YYYY-MM-DD HH:mm:ss'
+	        }),
+	    		myFormat
+	  		),
+		  	transports: [new transports.File({
+	      filename: log_name+".log",
+	    }),]
+		});
+		await logger.log(log_level, message);
+		const kafka = new Kafka({ clientId: this.clientId,
+    		brokers: this.brokers,
+    		moduleName: this.moduleName
+    		 })
+		const producer = kafka.producer()
+		
+		var day = dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss.l");
+	    await producer.connect()
+		await producer.send({
+				topic: "log-test",
+				messages: [
+					{
+						value: `{\"message\": \"${message}\",\"log_level\": \"${log_level}\", \"module_name\": \"${this.moduleName}\"}`,
+					},
+				],
+			})
+		await producer.disconnect()
+	}
 }
-
-
-async function log(message, log_level, log_name) {
-	const { createLogger, format, transports } = require('winston');
-	const { combine, timestamp, printf } = format;
-	
-	if(log_name === undefined){
-			log_name = "log"
-		}
-	if(log_level === undefined){
-			log_level = "info"
-		}
-	const myFormat = printf(({ level, message, timestamp }) => {
- 	 	return `${timestamp} ${level} : ${message}`;
-	});
-
-	
-	const logger = createLogger({
-	  	format: combine(
-    		timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-    		myFormat
-  		),
-	  	transports: [new transports.File({
-      filename: log_name+".log",
-    }),]
-	});
-	
-	console.log("%s", log_name)
-	logger.log(level=log_level, message);
-	producer(message, log_level)
-}
-
-log("hello", log_level="error")
---
+++ test.js
@@ -0,0 +1,8 @@
+const test = require('log')
+
+var myTest = test.Log(OUR CONFIG. SO GET OUT!)
+
+myTest.log("hello")
+
+
+	
